0c030f7b6fa48797e2f7dd04e0743572994032dd
David Frantz <d-frantz@web.de>
2020-08-22 14:41:43 +0200

    started working on aux tool to build layerstacks

---
+++ src/aux-level/_stack.c
@@ -0,0 +1,106 @@
+/**+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+This file is part of FORCE - Framework for Operational Radiometric 
+Correction for Environmental monitoring.
+
+Copyright (C) 2013-2020 David Frantz
+
+FORCE is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FORCE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FORCE.  If not, see <http://www.gnu.org/licenses/>.
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++**/
+
+/**+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+This program stacks images
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++**/
+
+#include <stdio.h>   // core input and output functions
+#include <stdlib.h>  // standard general utilities library
+
+#include "../cross-level/const-cl.h"
+#include "../cross-level/konami-cl.h"
+
+
+/** Geospatial Data Abstraction Library (GDAL) **/
+#include "gdal.h"           // public (C callable) GDAL entry points
+#include "cpl_conv.h"       // various convenience functions for CPL
+#include "cpl_string.h"       // various convenience functions for strings
+
+
+int main ( int argc, char *argv[] ){
+int b, nb;
+GDALDatasetH src, dst;
+GDALRasterBandH bsrc, bdst;
+char fsrc[NPOW_10], fdst[NPOW_10];
+char **meta  = NULL;
+char **bmeta = NULL;
+const char *bname = NULL;
+
+
+  if (argc >= 2) check_arg(argv[1]);
+  if (argc < 2){ printf("Usage: %s file1 file2 file3 etc.\n\n", argv[0]); return FAILURE;}
+
+
+  if (strlen(argv[1]) > NPOW_10-1){
+    printf("cannot copy, string too long.\n"); return FAILURE;
+  } else { strncpy(fsrc, argv[1], strlen(argv[1])); fsrc[strlen(argv[1])] = '\0';}
+  if (strlen(argv[2]) > NPOW_10-1){
+    printf("cannot copy, string too long.\n"); return FAILURE;
+  } else { strncpy(fdst, argv[2], strlen(argv[2])); fdst[strlen(argv[2])] = '\0';}
+
+
+  GDALAllRegister();
+
+  if ((src = GDALOpenEx(fsrc, GDAL_OF_READONLY, NULL, NULL, NULL)) == NULL){
+    printf("unable to open %s\n\n", fsrc); return FAILURE;}
+
+  if ((dst = GDALOpenEx(fdst, GDAL_OF_UPDATE,   NULL, NULL, NULL)) == NULL){
+    printf("unable to open %s\n\n", fdst); return FAILURE;}
+
+  if ((nb = GDALGetRasterCount(src)) != GDALGetRasterCount(dst)){
+    printf("src and dst images have different number of bands\n\n"); 
+    return FAILURE;}
+
+
+  // copy FORCE domain
+  meta = GDALGetMetadata(src, "FORCE");
+  //printf("Number of metadata items: %d\n", CSLCount(meta));
+  //CSLPrint(meta, NULL);
+  //CSLDestroy(meta);
+  GDALSetMetadata(dst, meta, "FORCE");
+
+  for (b=0; b<nb; b++){
+
+    bsrc = GDALGetRasterBand(src, b+1);
+    bdst = GDALGetRasterBand(dst, b+1);
+
+    // copy FORCE domain
+    bmeta = GDALGetMetadata(bsrc, "FORCE");
+    //printf("Number of metadata items in band %d: %d\n", b+1, CSLCount(bmeta));
+    //CSLPrint(bmeta, NULL);
+    //CSLDestroy(bmeta);
+    GDALSetMetadata(bdst, bmeta, "FORCE");
+
+    // copy bandname
+    bname = GDALGetDescription(bsrc);
+    GDALSetDescription(bdst, bname);
+
+  }
+
+  GDALClose(src);
+  GDALClose(dst);
+
+
+  return SUCCESS; 
+}
+
