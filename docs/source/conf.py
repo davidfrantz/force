cbfb8436e0af755f7612fe3eaf84ee667ec33ef9
David Frantz <david.frantz@uni-trier.de>
2021-11-05 16:56:19 +0100

    sphinx version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = u'3.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '4.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
