d66fbbdf36eda0f3233ff364e0588366236795c0
David Frantz <david.frantz@uni-trier.de>
2024-02-22 16:07:30 +0100

    refactored force-tile-extent, minimal usage change

--- bash/force-tile-extent.sh
+++ bash/force-tile-extent.sh
@@ -23,67 +23,125 @@
 ##########################################################################
 
 # functions/definitions ------------------------------------------------------------------
-export PROG=`basename $0`;
-export BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export MISC="$BIN/force-misc"
+PROG=$(basename "$0")
+BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+MISC="$BIN/force-misc"
+export PROG BIN MISC
 
 # source bash "library" file
 LIB="$MISC/force-bash-library.sh"
-eval ". ${LIB}" >/dev/null 2>&1 ;[[ "$?" -ne "0" ]] && echo "loading bash library failed" && exit 1;
-export LIB
-
-
-EXPECTED_ARGS=3
-
-if [ $# -ne $EXPECTED_ARGS ]
-then
-  echo "Usage: `basename $0` input-vector datacube-dir allow-list"
-  echo ""
-  echo "       input-file:   a polygon vector file"
-  echo "       datacube-dir: the directory of a datacube;"
-  echo "                     datacube-definition.prj needs to exist in there"
-  echo "       allow-list:   a tile allow-list to restrict the processing extent."
-  echo "                     This file will be written"
-  echo ""
-  exit
+if ! eval ". ${LIB}" >/dev/null 2>&1; then 
+  echo "loading bash library failed" 1>&2
+  exit 1
 fi
 
 
-INP=$1
-DIR=$2
-LIST=$3
+MANDATORY_ARGS=1
 
-if [ ! -r $INP ]; then
-  echo "$INP is not existing/readable"
-  exit
+export FORCE_CUBE_EXE="$BIN/force-cube"
+
+
+help(){
+cat <<HELP
+
+Usage: $PROG [-hvi] [-d datacube-dir] [-a allow-list] [-j jobs] input-vector
+
+  optional:
+  -h = show this help
+  -v = show version
+  -i = show program's purpose
+  
+  -d = directory of existing datacube
+       defaults to current directory
+       'datacube-definition.prj' needs to exist in there
+  -a = tile allow-list to restrict the processing extent
+       The extent will be computed based on the input AOI
+       Defaults to './allow-list.txt'
+  -j = number of jobs, defaults to 100%
+
+  Positional arguments:
+  - input-vector: a polygon vector file for the area of interest (AOI)
+
+  -----
+    see https://force-eo.readthedocs.io/en/latest/components/auxilliary/tile-extent.html
+
+HELP
+exit 1
+}
+export -f help
+
+# important, check required commands !!! dies on missing
+cmd_not_found "$FORCE_CUBE_EXE"
+
+
+# now get the options --------------------------------------------------------------------
+ARGS=$(getopt -o hvid:a:j: --long help,version,info,datacube:,allow:,jobs: -n "$0" -- "$@")
+if [ $? != 0 ] ; then help; fi
+eval set -- "$ARGS"
+
+DIR_CUBE="$PWD"
+FILE_ALLOW="$PWD/allow-list.txt"
+NJOB="100%"
+
+while :; do
+  case "$1" in
+    -h|--help) help ;;
+    -v|--version) force_version; exit 0;;
+    -i|--info) echo "Compute processing extent based on AOI"; exit 0;;
+    -d|--datacube) DIR_CUBE=$(readlink -f "$2"); shift ;;
+    -a|--allow) FILE_ALLOW=$(readlink -f "$2"); shift ;;
+    -j|--jobs) NJOB="$2"; shift ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+  shift
+done
+
+if [ $# -ne $MANDATORY_ARGS ] ; then 
+  echoerr "Mandatory argument is missing."; help
+else
+  FILE_AOI=$(readlink -f "$1")
 fi
 
-if [ ! -r $DIR/datacube-definition.prj ]; then
-  echo "$DIR/datacube-definition.prj is not existing/readable"
-  exit
-fi
+debug "datacube dir: $DIR_CUBE"
+debug "allow-list:   $FILE_ALLOW"
+debug "AOI:          $FILE_AOI"
+debug "njobs:        $NJOB"
+
+DIR_ALLOW=$(dirname "FILE_ALLOW")
+FILE_CUBE="$DIR_CUBE/datacube-definition.prj"
+
+# checks
+file_not_found "$FILE_AOI"
+dir_not_found "$DIR_CUBE"
+file_not_found "$FILE_CUBE"
+dir_not_writeable "$DIR_ALLOW"
 
 
-BASE=$(basename $INP)
-BASE=${BASE%%.*}
-TMP=$DIR"/"$BASE
-#echo $TMP
+# make temporary directory
+TIME=$(date +"%Y%m%d%H%M%S")
+BASE=$(basename "$FILE_AOI")
+DIR_TMP="$DIR_ALLOW/${BASE%%.*}_TEMP_$TIME"
+debug "DIR_TMP: $DIR_TMP"
+mkdir -p "$DIR_TMP"
+dir_not_writeable "$DIR_TMP" # safety check
+cp "$FILE_CUBE" -t "$DIR_TMP"
 
-mkdir -p $TMP
-cp $DIR/datacube-definition.prj $TMP/datacube-definition.prj
+# generate masks
+$FORCE_CUBE_EXE -o "$DIR_TMP" -b force-extent "$FILE_AOI" &> /dev/null
 
-# generate masks, use force-cube version relative to this program
-BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$BINDIR"/"force-cube -j 0 -s 10 -o $TMP -b force-extent $INP &> /dev/null
+# go to temporary directory
+DIR_NOW="$PWD"
+cd "$DIR_TMP" || {
+  echoerr "could not change to $DIR_TMP"
+  exit 1
+}
 
-
-WD=$PWD
-cd $TMP
 
 # find all the generated masks
-FILES=$(ls -d X*/*.tif)
-TILES=($(dirname $FILES))
-NTILES=${#TILES[*]}
+FILES_MASK=$(ls -d X*/*.tif)
+TILES_MASK=($(dirname $FILES_MASK))
+NTILES=${#TILES_MASK[*]}
 
 
 # compute range of tiles
@@ -92,7 +150,7 @@ XMAX=-999
 YMIN=9999
 YMAX=-999
 
-for i in "${TILES[@]}"; do
+for i in "${TILES_MASK[@]}"; do
 
   X=${i:1:4}
   Y=${i:7:4}
@@ -100,49 +158,33 @@ for i in "${TILES[@]}"; do
   X=$(echo $X | sed 's/^0*//')
   Y=$(echo $Y | sed 's/^0*//')
 
-  (( X > XMAX )) && XMAX=$X
-  (( X < XMIN )) && XMIN=$X
-  (( Y > YMAX )) && YMAX=$Y
-  (( Y < YMIN )) && YMIN=$Y
+  is_gt "$X" "$XMAX" && XMAX="$X"
+  is_lt "$X" "$XMIN" && XMIN="$X"
+  is_gt "$Y" "$YMAX" && YMAX="$Y"
+  is_lt "$Y" "$YMIN" && YMIN="$Y"
 
 done
 
-echo ""
-echo "Suggested Processing extent:"
-echo "X_TILE_RANGE =" $XMIN $XMAX
-echo "Y_TILE_RANGE =" $YMIN $YMAX
-echo ""
-
-
-# check if processing extent is square or not
-NSQX=$((XMAX-XMIN+1))
-NSQY=$((YMAX-YMIN+1))
-NSQ=$((NSQX*NSQY))
-
-if [ "$NTILES" -lt "$NSQ" ]; then
-
-  echo "Processing extent is not square."
-  echo "Suggest to use the tile allow-list:"
-  echo "FILE_TILE =" $LIST
-
-else
-
-  echo "Processing extent is square."
-  echo "Using the tile allow-list is not necessary, but can be used with:"
-  echo "FILE_TILE =" $LIST
-
-fi
-
-echo ""
-
-cd $WD
-
 # write allow-list
-echo $NTILES > $LIST
-printf "%s\n" "${TILES[@]}" >> $LIST
+echo "$NTILES" > "$FILE_ALLOW"
+printf "%s\n" "${TILES_MASK[@]}" >> "$FILE_ALLOW"
 
+# suggest parameters
+echo ""
+echo "Suggested Processing extent parameters:"
+echo "X_TILE_RANGE = $XMIN $XMAX"
+echo "Y_TILE_RANGE = $YMIN $YMAX"
+echo "FILE_TILE = $FILE_ALLOW"
+echo ""
 
-rm -r $TMP
+# change to previous dir
+cd "$DIR_NOW" || {
+  echoerr "could not change back to $DIR_NOW"
+  exit 1
+}
+
+# clean temporary data
+rm -r "$DIR_TMP"
 
 exit 0
 
