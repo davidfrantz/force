4b77cbd37bccbd8ab0de26a88566726ecc578168
David Frantz <david.frantz@uni-trier.de>
2024-07-18 15:26:45 +0200

    tests for fequal dequal

--- src/unit-testing/test_utils-cl.c
+++ src/unit-testing/test_utils-cl.c
@@ -6,6 +6,39 @@ void setUp(void) { }
 
 void tearDown(void) { }
 
+void test_fequal_Should_ReturnTrue_ForEqualFloats(void) {
+  TEST_ASSERT_TRUE(fequal(1.0f, 1.0f));
+  TEST_ASSERT_TRUE(fequal(0.0f, 0.0f));
+  TEST_ASSERT_TRUE(fequal(-1.0f, -1.0f));
+}
+
+void test_fequal_Should_ReturnTrue_ForNearlyEqualFloats(void) {
+  TEST_ASSERT_TRUE(fequal(1.0f, 1.0f + FLT_EPSILON / 2.0));
+  TEST_ASSERT_TRUE(fequal(1.0f, 1.0f - FLT_EPSILON / 2.0));
+}
+
+void test_fequal_Should_ReturnFalse_ForDifferentFloats(void) {
+  TEST_ASSERT_FALSE(fequal(1.0f, 2.0f));
+  TEST_ASSERT_FALSE(fequal(0.0f, FLT_EPSILON));
+}
+
+void test_dequal_Should_ReturnTrue_ForEqualDoubles(void) {
+  TEST_ASSERT_TRUE(dequal(1.0, 1.0));
+  TEST_ASSERT_TRUE(dequal(0.0, 0.0));
+  TEST_ASSERT_TRUE(dequal(-1.0, -1.0));
+}
+
+void test_dequal_Should_ReturnTrue_ForNearlyEqualDoubles(void) {
+  TEST_ASSERT_TRUE(dequal(1.0, 1.0 + DBL_EPSILON / 2.0));
+  TEST_ASSERT_TRUE(dequal(1.0, 1.0 - DBL_EPSILON / 2.0));
+}
+
+void test_dequal_Should_ReturnFalse_ForDifferentDoubles(void) {
+  TEST_ASSERT_FALSE(dequal(1.0, 2.0));
+  TEST_ASSERT_FALSE(dequal(0.0, DBL_EPSILON));
+}
+
+
 void test_num_decimal_places_SingleDigit(void) {
   TEST_ASSERT_EQUAL_INT(1, num_decimal_places(0));
   TEST_ASSERT_EQUAL_INT(1, num_decimal_places(5));
@@ -34,6 +67,14 @@ int main(void) {
 
   UNITY_BEGIN();
 
+  RUN_TEST(test_fequal_Should_ReturnTrue_ForEqualFloats);
+  RUN_TEST(test_fequal_Should_ReturnTrue_ForNearlyEqualFloats);
+  RUN_TEST(test_fequal_Should_ReturnFalse_ForDifferentFloats);
+  
+  RUN_TEST(test_dequal_Should_ReturnTrue_ForEqualDoubles);
+  RUN_TEST(test_dequal_Should_ReturnTrue_ForNearlyEqualDoubles);
+  RUN_TEST(test_dequal_Should_ReturnFalse_ForDifferentDoubles);
+  
   RUN_TEST(test_num_decimal_places_SingleDigit);
   RUN_TEST(test_num_decimal_places_MultipleDigit);
   RUN_TEST(test_num_decimal_places_NegativeNumbers);
