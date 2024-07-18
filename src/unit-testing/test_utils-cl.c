52615bdb00ff71c79dd8f1690232dab4ef731513
David Frantz <david.frantz@uni-trier.de>
2024-07-18 11:29:20 +0200

    updated unit test num_decimal_places

--- src/unit-testing/test_utils-cl.c
+++ src/unit-testing/test_utils-cl.c
@@ -1,27 +1,27 @@
 #include "unity/unity.h"
+#include <limits.h>
 #include "../cross-level/utils-cl.h"
-
 
 void setUp(void) { }
 
 void tearDown(void) { }
 
 void test_num_decimal_places_SingleDigit(void) {
-  TEST_ASSERT_EQUAL_INT(1, num_decimal_places(0);
-  TEST_ASSERT_EQUAL_INT(1, num_decimal_places(5);
+  TEST_ASSERT_EQUAL_INT(1, num_decimal_places(0));
+  TEST_ASSERT_EQUAL_INT(1, num_decimal_places(5));
 }
 
 void test_num_decimal_places_MultipleDigit(void) {
-  TEST_ASSERT_EQUAL(2, num_decimal_places(50));
-  TEST_ASSERT_EQUAL(3, num_decimal_places(500));
-  TEST_ASSERT_EQUAL(4, num_decimal_places(5000));
+  TEST_ASSERT_EQUAL_INT(2, num_decimal_places(50));
+  TEST_ASSERT_EQUAL_INT(3, num_decimal_places(500));
+  TEST_ASSERT_EQUAL_INT(4, num_decimal_places(5000));
 }
 
 void test_num_decimal_places_NegativeNumbers(void) {
-  TEST_ASSERT_EQUAL(1, num_decimal_places(-5));
-  TEST_ASSERT_EQUAL(2, num_decimal_places(-50));
-  TEST_ASSERT_EQUAL(3, num_decimal_places(-500));
-  TEST_ASSERT_EQUAL(4, num_decimal_places(-5000));
+  TEST_ASSERT_EQUAL_INT(1, num_decimal_places(-5));
+  TEST_ASSERT_EQUAL_INT(2, num_decimal_places(-50));
+  TEST_ASSERT_EQUAL_INT(3, num_decimal_places(-500));
+  TEST_ASSERT_EQUAL_INT(4, num_decimal_places(-5000));
 }
 
 void test_num_decimal_places_LargeNumbers(void) {
