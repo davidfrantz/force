299a1bbff5fa826faa6a56ccf843a85a1581d742
David Frantz <david.frantz@uni-trier.de>
2024-07-19 10:26:04 +0200

    alloc unit tests

--- src/unit-testing/test_utils-cl.c
+++ src/unit-testing/test_utils-cl.c
@@ -58,9 +58,9 @@ void test_num_decimal_places_NegativeNumbers(void) {
 }
 
 void test_num_decimal_places_LargeNumbers(void) {
-    TEST_ASSERT_EQUAL_INT(10, num_decimal_places(1234567890));
-    TEST_ASSERT_EQUAL_INT(10, num_decimal_places(INT_MAX));
-    TEST_ASSERT_EQUAL_INT(10, num_decimal_places(INT_MIN));
+  TEST_ASSERT_EQUAL_INT(10, num_decimal_places(1234567890));
+  TEST_ASSERT_EQUAL_INT(10, num_decimal_places(INT_MAX));
+  TEST_ASSERT_EQUAL_INT(10, num_decimal_places(INT_MIN));
 }
 
 int main(void) {
