daa05274a2b6e679f37f69ee5b613b065ebd4c12
David Frantz <david.frantz@uni-trier.de>
2024-12-20 11:12:18 +0100

    nvm, use actual RSR pointer already

--- src/modules/lower-level/meta-ll.c
+++ src/modules/lower-level/meta-ll.c
@@ -752,15 +752,9 @@ int svgrid = 5000;
         } else if (strcmp(sensor, "SEN2B") == 0){
           b_rsr = _RSR_START_SEN2B_;
         } else if (strcmp(sensor, "SEN2C") == 0){
-          #ifdef FORCE_DEBUG
-          printf("SEN2C: using SEN2A RSR. Adjust later!\n");
-          #endif
-          b_rsr = _RSR_START_SEN2A_;
+          b_rsr = _RSR_START_SEN2C_;
         } else if (strcmp(sensor, "SEN2D") == 0){
-          #ifdef FORCE_DEBUG
-          printf("SEN2D: using SEN2A RSR. Adjust later!\n");
-          #endif
-          b_rsr = _RSR_START_SEN2A_;
+          b_rsr = _RSR_START_SEN2D_;
         } else {
           printf("unknown/unsupported Sentinel-2! "); return FAILURE;
         }
