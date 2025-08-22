72b6a1fa8be06cad48756d4f20a321917a9f273b
David Frantz <david.frantz@uni-trier.de>
2025-08-22 16:00:35 +0200

    modularization of make targets with only necessary linking

---
+++ build/executables/force-stratified-sample.mk
@@ -0,0 +1,22 @@
+force-stratified-sample: \
+  alloc-cl \
+  dir-cl \
+  konami-cl \
+  stats-cl \
+  string-cl \
+  sys-cl \
+  table-cl \
+  utils-cl \
+  $(EXE_AUX_DIR)/force-stratified-sample.c
+	$(G11) -o $(BINDIR)/force-stratified-sample $(EXE_AUX_DIR)/force-stratified-sample.c \
+	$(OBJDIR)/alloc-cl.o \
+	$(OBJDIR)/dir-cl.o \
+	$(OBJDIR)/konami-cl.o \
+	$(OBJDIR)/stats-cl.o \
+	$(OBJDIR)/string-cl.o \
+	$(OBJDIR)/sys-cl.o \
+	$(OBJDIR)/table-cl.o \
+	$(OBJDIR)/utils-cl.o \
+	$(GDAL_INCLUDES) $(GDAL_FLAGS) $(GDAL_LIBS) \
+	$(GSL_INCLUDES) $(GSL_FLAGS) $(GSL_LIBS) \
+	-lm
---
+++ build/executables/force-tabulate-grid.mk
@@ -0,0 +1,22 @@
+force-tabulate-grid: \
+  alloc-cl \
+  cube-cl \
+  dir-cl \
+  konami-cl \
+  lock-cl \
+  string-cl \
+  sys-cl \
+  utils-cl \
+  warp-cl \
+  $(EXE_AUX_DIR)/force-tabulate-grid.c
+	$(G11) -o $(BINDIR)/force-tabulate-grid $(EXE_AUX_DIR)/force-tabulate-grid.c \
+	$(OBJDIR)/alloc-cl.o \
+	$(OBJDIR)/cube-cl.o \
+	$(OBJDIR)/dir-cl.o \
+	$(OBJDIR)/konami-cl.o \
+	$(OBJDIR)/lock-cl.o \
+	$(OBJDIR)/string-cl.o \
+	$(OBJDIR)/sys-cl.o \
+	$(OBJDIR)/utils-cl.o \
+	$(OBJDIR)/warp-cl.o \
+	$(GDAL_INCLUDES) $(GDAL_FLAGS) $(GDAL_LIBS)
