98f781f6a3090cb0c19eb25206a4902152f79a8c
Velarde Jara <jara@uni-trier.de>
2025-09-24 15:56:27 +0200

    adding reverse instruction on the else statement and stronger replacement rules

--- debug.sh
+++ debug.sh
@@ -52,9 +52,10 @@ fi
 
 if [ $enable -eq 1 ]; then
   sed -i -e 's%^[/]*\(#define FORCE_DEBUG\)%\1%' src/modules/cross-level/const-cl.h
-  sed -i 's/^export DEBUG=false$/export DEBUG=true/' misc/force-bash-library.sh
+  sed -i 's/^export DEBUG=.*$/export DEBUG=true/' misc/force-bash-library.sh
 elif [ $enable -eq 0 ]; then
   sed -i -e 's%^[/]*\(#define FORCE_DEBUG\)%//\1%' src/modules/cross-level/const-cl.h
+  sed -i 's/^export DEBUG=.*$/export DEBUG=false/' misc/force-bash-library.sh
 fi
 
 exit 0
