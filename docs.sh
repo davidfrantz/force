f64d8d034f94635706896379e798093806d4c3b0
David Frantz <david.frantz@uni-trier.de>
2025-12-19 14:10:51 +0100

    auto-generate runtime data prints to be used in docs

--- docs.sh
+++ docs.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-make -j force-parameter
+make -j force-parameter force-runtime-data misc
 
 MOD=$(bin/force-parameter -m | grep -v 'available modules' | sed 's/^ *//' | cut -d ':' -f 1)
 
 for m in $MOD; do
   bin/force-parameter "docs/source/_static/parameter-files/parameter_$m.prm" "$m"
 done
+
+bin/force-runtime-data -s > docs/source/_static/runtime-data/sensors.txt
+bin/force-runtime-data -x > docs/source/_static/runtime-data/indices.txt
 
 make clean
 
