46b70048f3e068708949d58a48c68ab45517e15e
David Frantz <david.frantz@uni-trier.de>
2026-03-17 10:56:01 +0100

    removed force-lut-modis, and curl

--- Makefile
+++ Makefile
@@ -36,10 +36,6 @@ GSL_INCLUDES = $(shell gsl-config --cflags)
 GSL_LIBS = $(shell gsl-config --libs)
 GSL_FLAGS = -Wl,-rpath=/usr/lib/x86_64-linux-gnu -DHAVE_INLINE=1 -DGSL_RANGE_CHECK=0
 
-CURL_INCLUDES = -I/usr/include/curl -I/usr/include/x86_64-linux-gnu/curl
-CURL_LIBS = -L/usr/lib/x86_64-linux-gnu -lcurl
-CURL_FLAGS = -Wl,-rpath=/usr/lib/x86_64-linux-gnu
-
 OPENCV_INCLUDES = $(shell pkg-config --cflags opencv4)
 OPENCV_LIBS = $(shell pkg-config --libs opencv4)
 OPENCV_FLAGS = -Wl,-rpath=/usr/local/lib
--- build/executables.mk
+++ build/executables.mk
@@ -12,7 +12,6 @@ include $(BUILDDIR)/executables/force-hist.mk
 include $(BUILDDIR)/executables/force-import-modis.mk
 include $(BUILDDIR)/executables/force-info.mk
 include $(BUILDDIR)/executables/force-l2ps.mk
-include $(BUILDDIR)/executables/force-lut-modis.mk
 include $(BUILDDIR)/executables/force-map-accuracy.mk
 include $(BUILDDIR)/executables/force-mdcp.mk
 include $(BUILDDIR)/executables/force-parameter.mk
@@ -31,7 +30,6 @@ exe: \
   force-import-modis \
   force-info \
   force-l2ps \
-  force-lut-modis \
   force-map-accuracy \
   force-mdcp \
   force-parameter \
